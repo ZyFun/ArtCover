de7c893c119d39c82e85507edd25c1705bbe5f3c
Dmitriy Danilin <danilindn@icloud.com>
2020-07-17 19:07:55 +0300

    Initial Commit

---
+++ ArtCover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtCover
+//
+//  Created by Дмитрий Данилин on 17.07.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
